2ac69daf7252903c41429e7bcd0b87eb8b8ef0d9
stanoz <s.oziemczuk@gmail.com>
2026-08-01 12:48:50 +0200

    set annotations

--- backend/src/main/java/com/timeline/view/backend/model/entity/Event.java
+++ backend/src/main/java/com/timeline/view/backend/model/entity/Event.java
@@ -1,13 +1,20 @@
 package com.timeline.view.backend.model.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document(collation = "events")
+@Builder
+@Data
 public class Event {
     @Id
+    @Setter(AccessLevel.NONE)
     private String id;
 
     private String description;
