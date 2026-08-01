b9afb42f45e87110f4970b63317f7473a1182540
stanoz <s.oziemczuk@gmail.com>
2026-08-01 12:52:57 +0200

    add event repository

---
+++ backend/src/main/java/com/timeline/view/backend/repositories/EventRepository.java
@@ -0,0 +1,12 @@
+package com.timeline.view.backend.repositories;
+
+import com.timeline.view.backend.model.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends MongoRepository<Event, String> {
+    Page<Event> findAllOrderByDateTime(Pageable pageable);
+}
