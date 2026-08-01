3ee99605bc7c8a5b28120ec68fa69b914f558bfa
stanoz <s.oziemczuk@gmail.com>
2026-08-01 13:07:16 +0200

    add event controller

---
+++ backend/src/main/java/com/timeline/view/backend/controllers/EventController.java
@@ -0,0 +1,26 @@
+package com.timeline.view.backend.controllers;
+
+import com.timeline.view.backend.model.dtos.EventDto;
+import com.timeline.view.backend.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+    private static final String DEFAULT_PAGE_SIZE = "4";
+
+    private final EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDto>> getEventsByDate(@RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) int size) {
+        return ResponseEntity.ok(eventService.getPaginatedEventsByDates(page, size));
+    }
+}
